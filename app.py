9f1435aecb0e6aff9de26782e4e46602271d97b5
romieza24-meet <139688912+romieza24-meet@users.noreply.github.com>
2023-07-27 05:57:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,38 @@ import pyrebase
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
+config = {
+    "apiKey": "AIzaSyC05n1tnpMMBpMna5SVTDH9deLVA9zIrss",
+    "authDomain": "travlerpack-ec5ab.firebaseapp.com",
+    "databaseURL": "https://travlerpack-ec5ab-default-rtdb.firebaseio.com",
+    "projectId": "travlerpack-ec5ab",
+    "storageBucket": "travlerpack-ec5ab.appspot.com",
+    "messagingSenderId": "1017759641",
+    "appId": "1:1017759641:web:fc578430845db10e3c3508",
+    "measurementId": "G-B2YPMZ3MKN",
+}
+
+fb = pyrebase.initialize_app(config)
+auth = fb.auth()
+db = fb.database()
+
 #Code goes below here
+
+
+@app.route('/zakat', methods=['GET', 'POST'])
+def zakat():
+    money = ''
+    zakat = ''
+    if request.method == 'POST':
+        try:
+            int(money) = request.form['zakat']
+            zakat = money*0.025
+            db.child("zakat").update(history)
+            zakat = db.child("zakat").get().val()
+        except:
+            zakat = 'Error'
+        return render_template('index.html', zakat = zakat)
+    return render_template('index.html')
 
 
 
